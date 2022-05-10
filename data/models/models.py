a983a138c31417547dced197ff6da345e855b183
Mehrnoosh H. Kashani <mehrnoosh.haghighat@gmail.com>
2022-05-10 18:46:49 +0430

    define db models

---
+++ data/models/models.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String, DateTime, JSON, ForeignKey
+from sqlalchemy.orm import declarative_base, relationship
+
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    email = Column(String(255), nullable=False)
+    password = Column(String(255), nullable=False)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow)
+
+
+class UserSheets(Base):
+    __tablename__ = 'user_sheets'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    sheet_name = Column(String, nullable=False)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow)
+    sheet_filds = Column(JSON, nullable=False)
+
+    user = relationship('User', back_populates='user_sheets')
+
