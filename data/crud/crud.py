dce205e2cf59fd6760d17ef47464e8b622ede512
Mehrnoosh H. Kashani <mehrnoosh.haghighat@gmail.com>
2022-05-11 19:56:01 +0430

    Implement crud operation for courses

--- data/crud/crud.py
+++ data/crud/crud.py
@@ -1,4 +1,4 @@
-from ..models.models import Base, User, UserCources
+from ..models.models import Base, User, UserCourse
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
@@ -8,6 +8,8 @@ from config import db_config
 engine = create_engine(db_config['URI'])
 Base.metadata.create_all(engine)
 
+
+# Create a new user in database
 def db_create_user(email, password):
     with Session(bind=engine) as session:
         user = User(email=email, password=password)
@@ -17,32 +19,54 @@ def db_create_user(email, password):
     return user.id
 
 
+# Retrieve a user from database by id
 def db_get_user_by_id(user_id):
     with Session(bind=engine) as session:
         user = session.query(User).filter(User.id == user_id).first()
     return user
 
 
+# Retrieve a user from database by email
 def db_get_user_by_email(user_email):
     with Session(bind=engine) as session:
         user = session.query(User).filter(User.email == user_email).first()
     return user
 
+
+# Retrieve list of all users in database
 def db_get_users():
     with Session(bind=engine) as session:
         users = session.query(User).all()
     return users
 
 
-def db_create_user_cource(user_id, cource_name, cource_filds):
+def db_create_user_course(user_id, course_name, course_filds):
     with Session(bind=engine) as session:
-        user_cource = UserCources(
+        user_course = UserCourse(
             user_id=user_id,
-            cource_name=cource_name,
-            cource_filds=cource_filds,
-            cource_info=None,
+            course_name=course_name,
+            course_filds=course_filds,
+            course_info=None,
         )
-        session.add(user_cource)
+        session.add(user_course)
         session.commit()
-        session.refresh(user_cource)
-    return user_cource.id
+        session.refresh(user_course)
+    return user_course.id
+
+
+def db_get_user_courses(user_id):
+    with Session(bind=engine) as session:
+        user_courses = session.query(UserCourse).filter(UserCourse.user_id == user_id).all()
+    return user_courses
+
+
+def db_get_course_by_id(course_id):
+    with Session(bind=engine) as session:
+        course = session.query(UserCourse).filter(UserCourse.id == course_id).first()
+    return course
+
+
+def db_update_course(course_id, course_info):
+    with Session(bind=engine) as session:
+        course = session.query(UserCourse).filter(UserCourse.id == course_id).first()
+        course.course_info = course_info
