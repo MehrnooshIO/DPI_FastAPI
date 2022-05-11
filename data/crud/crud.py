f0b550e8687c1fe77507f8e4e9d67b38b5380021
Mehrnoosh H. Kashani <mehrnoosh.haghighat@gmail.com>
2022-05-11 11:16:52 +0430

    Add get users from db and save cources to db

--- data/crud/crud.py
+++ data/crud/crud.py
@@ -1,4 +1,4 @@
-from ..models.models import Base, User
+from ..models.models import Base, User, UserCources
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
@@ -27,3 +27,22 @@ def db_get_user_by_email(user_email):
     with Session(bind=engine) as session:
         user = session.query(User).filter(User.email == user_email).first()
     return user
+
+def db_get_users():
+    with Session(bind=engine) as session:
+        users = session.query(User).all()
+    return users
+
+
+def db_create_user_cource(user_id, cource_name, cource_filds):
+    with Session(bind=engine) as session:
+        user_cource = UserCources(
+            user_id=user_id,
+            cource_name=cource_name,
+            cource_filds=cource_filds,
+            cource_info=None,
+        )
+        session.add(user_cource)
+        session.commit()
+        session.refresh(user_cource)
+    return user_cource.id
