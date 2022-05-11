82c6ecff99e79df66df10656e27b718aaa8ca2bf
Mehrnoosh H. Kashani <mehrnoosh.haghighat@gmail.com>
2022-05-11 11:17:22 +0430

    Define cources database table

--- data/models/models.py
+++ data/models/models.py
@@ -14,14 +14,16 @@ class User(Base):
     password = Column(String(255), nullable=False)
     created_at = Column(DateTime, nullable=False, default=datetime.utcnow)
 
+    user_cources = relationship('UserCources', back_populates='user')
 
-class UserSheets(Base):
-    __tablename__ = 'user_sheets'
+
+class UserCources(Base):
+    __tablename__ = 'user_cources'
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
-    sheet_name = Column(String, nullable=False)
+    cource_name = Column(String, nullable=False)
     created_at = Column(DateTime, nullable=False, default=datetime.utcnow)
-    sheet_filds = Column(JSON, nullable=False)
+    cource_filds = Column(JSON, nullable=False)
+    cource_info = Column(JSON, nullable=True)
 
-    user = relationship('User', back_populates='user_sheets')
-
+    user = relationship('User', back_populates='user_cources')
