6a0f55b5b3a9b6210aa6a3e42cdbea87a08c6e4e
Mehrnoosh H. Kashani <mehrnoosh.haghighat@gmail.com>
2022-05-11 11:18:30 +0430

    Add auth to user routes

--- routes/user_routes/user_routes.py
+++ routes/user_routes/user_routes.py
@@ -1,9 +1,13 @@
-from email.policy import HTTP
-from fastapi import APIRouter, HTTPException
-from helper.encrypt import encrypt_password, check_password, create_token
+from fastapi import APIRouter, Depends, HTTPException
+from helper.encrypt import encrypt_password, check_password, create_token, get_user_id_from_token
+from fastapi.security import OAuth2PasswordBearer
 
 from data.schemas.user_schema import UserSchema
 from data.crud import crud
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/token")
+
 
 def auth_router() -> APIRouter:
     user_router = APIRouter()
@@ -14,7 +18,7 @@ def auth_router() -> APIRouter:
         user_id = crud.db_create_user(user_input.email, hashed_pass)
         return {"user_id": user_id}
 
-    @user_router.post("/login")
+    @user_router.post("/token")
     def login(user_input: UserSchema):
         user = crud.db_get_user_by_email(user_input.email)
         if not user:
@@ -25,6 +29,15 @@ def auth_router() -> APIRouter:
         else:
             token = create_token(user.id)
             return {"jwt": token}
-          
+
+    @user_router.get("/users")
+    def get_users(token: str = Depends(oauth2_scheme)):
+        users = crud.db_get_users()
+        return users
+
+    @user_router.get("/users/me")
+    def read_users_me(id: int = Depends(get_user_id_from_token)):
+        user = crud.db_get_user_by_id(id)
+        return user
 
     return user_router
